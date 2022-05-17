ab2c80b00309e0060d4bee58cc6927aefa3a01da
Leo <leonardo.loza.20isc@tecsanpedro.edu.mx>
2022-05-17 13:14:40 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace TAP_U2_EJEMPLO2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbOpciones = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnFoto = new System.Windows.Forms.Button();
+            this.btnCamara = new System.Windows.Forms.Button();
+            this.cbVideoCamara = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnLeerQR = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbOpciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbOpciones
+            // 
+            this.cbOpciones.Controls.Add(this.textBox1);
+            this.cbOpciones.Controls.Add(this.btnLeerQR);
+            this.cbOpciones.Controls.Add(this.button2);
+            this.cbOpciones.Controls.Add(this.btnFoto);
+            this.cbOpciones.Controls.Add(this.btnCamara);
+            this.cbOpciones.Controls.Add(this.cbVideoCamara);
+            this.cbOpciones.Location = new System.Drawing.Point(39, 28);
+            this.cbOpciones.Name = "cbOpciones";
+            this.cbOpciones.Size = new System.Drawing.Size(794, 132);
+            this.cbOpciones.TabIndex = 0;
+            this.cbOpciones.TabStop = false;
+            this.cbOpciones.Text = "Opciones";
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::TAP_U2_EJEMPLO2.Properties.Resources.stop_FILL0_wght400_GRAD0_opsz48;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("a Anti Corona", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(476, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = " Detener";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFoto
+            // 
+            this.btnFoto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFoto.Font = new System.Drawing.Font("a Anti Corona", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFoto.Image = global::TAP_U2_EJEMPLO2.Properties.Resources.baseline_face_black_18dp;
+            this.btnFoto.Location = new System.Drawing.Point(322, 51);
+            this.btnFoto.Name = "btnFoto";
+            this.btnFoto.Size = new System.Drawing.Size(103, 68);
+            this.btnFoto.TabIndex = 2;
+            this.btnFoto.Text = "Foto";
+            this.btnFoto.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnFoto.UseVisualStyleBackColor = true;
+            this.btnFoto.Click += new System.EventHandler(this.btnFoto_Click);
+            // 
+            // btnCamara
+            // 
+            this.btnCamara.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCamara.Font = new System.Drawing.Font("a Anti Corona", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCamara.Image = global::TAP_U2_EJEMPLO2.Properties.Resources.baseline_video_call_black_18dp;
+            this.btnCamara.Location = new System.Drawing.Point(23, 51);
+            this.btnCamara.Name = "btnCamara";
+            this.btnCamara.Size = new System.Drawing.Size(99, 68);
+            this.btnCamara.TabIndex = 1;
+            this.btnCamara.Text = " Iniciar";
+            this.btnCamara.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnCamara.UseVisualStyleBackColor = true;
+            this.btnCamara.Click += new System.EventHandler(this.btnCamara_Click);
+            // 
+            // cbVideoCamara
+            // 
+            this.cbVideoCamara.FormattingEnabled = true;
+            this.cbVideoCamara.Location = new System.Drawing.Point(6, 21);
+            this.cbVideoCamara.Name = "cbVideoCamara";
+            this.cbVideoCamara.Size = new System.Drawing.Size(581, 24);
+            this.cbVideoCamara.TabIndex = 0;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(39, 179);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(587, 224);
+            this.videoSourcePlayer1.TabIndex = 1;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.Click += new System.EventHandler(this.videoSourcePlayer1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnLeerQR
+            // 
+            this.btnLeerQR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLeerQR.Font = new System.Drawing.Font("a Anti Corona", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeerQR.Image = global::TAP_U2_EJEMPLO2.Properties.Resources.baseline_face_black_18dp;
+            this.btnLeerQR.Location = new System.Drawing.Point(145, 51);
+            this.btnLeerQR.Name = "btnLeerQR";
+            this.btnLeerQR.Size = new System.Drawing.Size(103, 68);
+            this.btnLeerQR.TabIndex = 4;
+            this.btnLeerQR.Text = "Leer QR";
+            this.btnLeerQR.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLeerQR.UseVisualStyleBackColor = true;
+            this.btnLeerQR.Click += new System.EventHandler(this.btnLeerQR_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(610, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(995, 415);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Controls.Add(this.cbOpciones);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cbOpciones.ResumeLayout(false);
+            this.cbOpciones.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox cbOpciones;
+        private System.Windows.Forms.ComboBox cbVideoCamara;
+        private System.Windows.Forms.Button btnCamara;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnFoto;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button btnLeerQR;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;//La voy a usar para bitmap
+using System.Windows.Forms;
+//Agregar las librerías de video
+using AForge.Video;
+using BarcodeLib.BarcodeReader;
+using System.Drawing.Imaging;//Para hacer la foto 
+using AForge.Video.DirectShow;
+
+namespace TAP_U2_EJEMPLO2
+{
+    public partial class Form1 : Form
+    {
+        //Variables globales - que dispositivos tneemos para grabar
+        FilterInfoCollection filterInfo; //clase para enlistar las camaras que tengo disponibles
+        //Clase para usar la camara
+        VideoCaptureDevice videoCapture;
+        //videocapturedeviceform se usa cuando ya se tiene funcional en el formulario
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Conocer los dispostivos de video disponibles
+            filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo infoCamaras in filterInfo)//se cambia el tipo de dato de los elementos que se
+                //guardan en el foreach
+            {
+                cbVideoCamara.Items.Add(infoCamaras.Name);
+            }
+        }
+
+        private void videoSourcePlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCamara_Click(object sender, EventArgs e)
+        {
+            //Acceder a la cámara
+            videoCapture = new VideoCaptureDevice(filterInfo[cbVideoCamara.SelectedIndex].MonikerString);
+            //el monikerstring hace que el indice seleccionado lo transforma o relaciona al numero de
+            //puerto que esta conecado esa camara que selecciona de la placa madre
+            videoSourcePlayer1.VideoSource = videoCapture;
+            videoSourcePlayer1.Start();
+ 
+            //el index me pasa el nombre de la camara
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer1.SignalToStop();
+        }
+
+        private void btnFoto_Click(object sender, EventArgs e)
+        {
+            //Guardar una foto tomada con la WebCam - cuadro de dialogo si queremos guardar el archivo en
+            //una ruta específica
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            //ya tengo un objeto llamado fileDialog
+            fileDialog.Filter = "Imagen JPG | *.jpg";
+            //Se va a guardar con la extensión jpg
+
+
+            //Guardar la imagen
+            //Primero tengo que garantizar que tenga un nombre
+            if (fileDialog.FileName != null)
+            {
+                //El FileName es el nombre del archivo
+
+                //Para capturar imagenes utilizamos bitmap - se captura la imagen de la camara
+                Bitmap img = videoSourcePlayer1.GetCurrentVideoFrame();
+                //Guardar la imagen
+                //La propiead filename agarra el nombre que el usuario escriió 
+                
+                //Guarda el archivo en la ruta que escogí con el nombre que yo escriba y con el formato que yo escogí
+                img.Save(fileDialog.FileName, ImageFormat.Jpeg);
+                //Libreramos la memoria
+                img.Dispose();
+
+                    //Las imagenes se llaman como img
+            }
+
+
+            //Mostrar el fileDialog
+            fileDialog.ShowDialog();
+
+
+            
+                //todo lo que diga dialog es un cuadro contextual que aparece aparte del form y nos
+                //va a dar opciones de respuesta
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Por medio de la camaraa, leer un QR o un Barcode
+              //Es el obeto que recibe la señal de video, vamos a checar si está recbiendo video
+            if (videoSourcePlayer1.GetCurrentVideoFrame ()!= null)
+            {
+                //Con el bitmap estoy cambiando cada segundo la imagen y se guarda
+                //No se crea un objeto cada tantos, solo va cambiando la foto
+                Bitmap img = new Bitmap(videoSourcePlayer1.GetCurrentVideoFrame());
+
+                //Creo un arreglo para tener informacion de distintas partes
+                //Checo que tipo de cosa quiero leer, en este cso un qr
+                //BarcodeReader me va a separar la información consultada
+                string[] resultadoLectura = BarcodeReader.read(img, BarcodeReader.QRCODE);
+
+                //garantizamos que solo haya un objeto eliminando lo contenido
+                img.Dispose();
+
+                if (resultadoLectura != null )
+                {
+                    textBox1.Text = resultadoLectura[0];
+
+                }
+
+            }
+        }
+
+        private void btnLeerQR_Click(object sender, EventArgs e)
+        {
+            //prendo el timer de la cámara
+            timer1.Enabled = true;
+
+        }
+    }
+} 
